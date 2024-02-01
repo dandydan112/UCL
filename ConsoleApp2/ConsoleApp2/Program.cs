944d13b1e6c38e60e7b35f80f74b3523d9de7ee3
dandydan112 <dan.t.nguyen123@gmail.com>
2024-02-01 09:08:38 +0100

    multiple chioce eksamen push

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pik = "2";
+
+            int pik = int.Parse(pik);
+
+            Console.WriteLine(pik + 2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
