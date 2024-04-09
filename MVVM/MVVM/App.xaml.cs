db35da40561a5fc3c9733c66239677a5e3aebd1b
dandydan112 <dan.t.nguyen123@gmail.com>
2024-04-09 12:58:37 +0200

    MVVM

---
+++ MVVM/MVVM/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MVVM.ViewModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MVVM.MainWindow window = new MainWindow();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+
+}
