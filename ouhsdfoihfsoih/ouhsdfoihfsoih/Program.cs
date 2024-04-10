c40cadbe51d57d77f54e6760c6e13e5d1537f91b
dandydan112 <dan.t.nguyen123@gmail.com>
2024-04-10 11:54:38 +0200

    MVVM

---
+++ ouhsdfoihfsoih/ouhsdfoihfsoih/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ouhsdfoihfsoih
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 20;
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
