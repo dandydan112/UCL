71263e560357b9bf5851cf9f65cd93b0a3797600
dandydan112 <dan.t.nguyen123@gmail.com>
2024-10-08 10:36:17 +0200

    3 projects deleted

--- BankWebApplication/BankWebApplication/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BankWebApplication.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
