4ff5c89fc5b79220911c80c0af1d382444c5557d
dandydan112 <dan.t.nguyen123@gmail.com>
2024-10-08 10:29:59 +0200

    commit

--- CORSWebApplication/Program.cs
+++ CORSWebApplication/Program.cs
@@ -3,7 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyAllowedOrigins",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:8081") // note the port is included 
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +25,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+app.UseCors("MyAllowedOrigins");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- CORSWebApplication/wwwroot/js/site.js
+++ CORSWebApplication/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function FetchRemote(remoteHost, path, httpMethod) {
+    const remoteResponse = document.getElementById('remoteResponse');
+
+    fetch(`${remoteHost}${path}`,
+        {
+            method: httpMethod,
+        }).then(response => {
+            if (response.ok) {
+                response.text().then(text => {
+                    remoteResponse.innerText = text;
+                });
+            }
+            else {
+                remoteResponse.innerText = response.status;
+            }
+        })
+        .catch(() => remoteResponse.innerText = 'An error occurred, might be CORS?! :) Press F12 to open the web debug tools');
+}
